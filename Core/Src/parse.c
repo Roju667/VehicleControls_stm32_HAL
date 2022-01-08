a49a5f4fb97cbfe37ae10fa00a6e89d3bee7198f
Roju667 <pawel.rojek94@gmail.com>
2022-01-08 21:08:46 +0100

    last push v2

--- Core/Src/parse.c
+++ Core/Src/parse.c
@@ -15,57 +15,68 @@
 #include "movement_control.h"
 #include "parse.h"
 
-
-
 /*
  * @ function parse message and start command procedures
  */
-uint8_t Parser_Parse(uint8_t *ParseBuffer, Servo_t *ServoX, Motor_t *MotorY,
-		VehicleLED_t *VehicleLEDs)
+uint8_t Parser_Parse(uint8_t *ParseBuffer, Servo_t *ServoX, Motor_t *MotorY, VehicleLED_t *VehicleLEDs)
 {
 	uint8_t *ParsePointer;
 
-	ParsePointer = (uint8_t*)(strtok((char*)ParseBuffer, ";"));
+	ParsePointer = (uint8_t*) (strtok((char*) ParseBuffer, ";"));
 
-	if(strcmp((char*)ParsePointer,"ACK") == 0)
+	if (strcmp((char*) ParsePointer, "ACK") == 0)
 	{
 		// light up green led
 		VehicleLEDs->ControlWord |= (0x01 << LED_COMM_OFFSET);
-		// write servo position
-		ParsePointer = (uint8_t*)(strtok(NULL, ";"));
-		ServoX->PWMCommandNew = atoi((const char*) ParsePointer);
 
-		// write motor speed
-		ParsePointer = (uint8_t*)(strtok(NULL, ";"));
-		MotorY->PWMCommandNew = atoi((const char*) ParsePointer);
+		for (uint8_t i = 0; i < 4; i++)
+		{
 
-		ParsePointer = (uint8_t*) (strtok(NULL, ";"));
-		if (strcmp((char*) ParsePointer, "1") == 0)
-		{
-			// set lights on
-			VehicleLEDs->ControlWord |= (0x01 << LED_FRONT1_OFFSET);
-			VehicleLEDs->ControlWord |= (0x01 << LED_FRONT2_OFFSET);
-		}
-		else
-		{
-			// reset lights
-			VehicleLEDs->ControlWord &= ~(0x01 << LED_FRONT1_OFFSET);
-			VehicleLEDs->ControlWord &= ~(0x01 << LED_FRONT2_OFFSET);
+			ParsePointer = (uint8_t*) (strtok(NULL, ";"));
+
+			switch (i)
+			{
+
+			case 1: // write servo position
+				ServoX->PWMCommandNew = atoi((const char*) ParsePointer);
+				break;
+
+			case 2: // write motor speed
+				MotorY->PWMCommandNew = atoi((const char*) ParsePointer);
+				break;
+
+			case 3: // control LED
+				if (strcmp((char*) ParsePointer, "1") == 0)
+				{
+					// set lights on
+					VehicleLEDs->ControlWord |= (0x01 << LED_FRONT1_OFFSET);
+					VehicleLEDs->ControlWord |= (0x01 << LED_FRONT2_OFFSET);
+				}
+				else
+				{
+					// reset lights
+					VehicleLEDs->ControlWord &= ~(0x01 << LED_FRONT1_OFFSET);
+					VehicleLEDs->ControlWord &= ~(0x01 << LED_FRONT2_OFFSET);
+				}
+				break;
+			case 4: // control motor
+
+				// check if button motor on is pressed
+				// if yes allow movement of servo and motor
+				if (strcmp((char*) ParsePointer, "1") == 0)
+				{
+					ServoX->ContorlOn = 1;
+					MotorY->ControlOn = 1;
+				}
+				else
+				{
+					ServoX->ContorlOn = 0;
+					MotorY->ControlOn = 0;
+				}
+			}
+
 		}
 
-		// check if button motor on is pressed
-		// if yes allow movement of servo and motor
-		ParsePointer = (uint8_t*) (strtok(NULL, ";"));
-		if (strcmp((char*) ParsePointer, "1") == 0)
-		{
-			ServoX->ContorlOn = 1;
-			MotorY->ControlOn = 1;
-		}
-		else
-		{
-			ServoX->ContorlOn = 0;
-			MotorY->ControlOn = 0;
-		}
 	}
 
 	return 0;
